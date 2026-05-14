0e7b6074c26e16262c7330915ac26d9f1f47b245
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 01:23:22 +0530

    fixed websocket

--- main.py
+++ main.py
@@ -1,53 +1,47 @@
 from dhanhq import DhanContext, MarketFeed
-from datetime import datetime
 import os
 
-# ==================================
+# =========================
 # DHAN CREDENTIALS
-# ==================================
-
-CLIENT_CODE = os.getenv("DHAN_CLIENT_CODE")
+# =========================
+CLIENT_ID = os.getenv("DHAN_CLIENT_CODE")
 ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
 
-# ==================================
-# DHAN CONNECTION
-# ==================================
-
-dhan_context = DhanContext(
-    CLIENT_CODE,
-    ACCESS_TOKEN
-)
+# =========================
+# DHAN CONTEXT
+# =========================
+dhan_context = DhanContext(CLIENT_ID, ACCESS_TOKEN)
 
 print("================================")
 print("🟢 DHAN CONNECTED SUCCESSFULLY")
 print("================================")
 
-# ==================================
-# BANKNIFTY LIVE FEED
-# ==================================
-
+# =========================
+# BANKNIFTY INSTRUMENT
+# =========================
 instruments = [
-    (MarketFeed.IDX, "25", MarketFeed.Ticker)
+    (MarketFeed.IDX_I, "25", MarketFeed.Ticker)
 ]
 
-version = "v2"
-
+# =========================
+# CREATE FEED
+# =========================
 feed = MarketFeed(
     dhan_context,
     instruments,
-    version
+    version="v2"
 )
 
-# ==================================
+# IMPORTANT
+feed.run_forever()
+
+# =========================
 # LIVE DATA LOOP
-# ==================================
-
+# =========================
 while True:
-
     response = feed.get_data()
 
     print("================================")
-    print("⏰ TIME :", datetime.now().strftime("%H:%M:%S"))
-    print("📈 BANKNIFTY LIVE DATA")
+    print("📈 LIVE BANKNIFTY DATA")
     print(response)
     print("================================")
