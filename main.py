476fe75c488b077beb411cf746eff6a0164af064
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 01:15:13 +0530

    Dhan 2.22

--- main.py
+++ main.py
@@ -3,57 +3,39 @@ from datetime import datetime
 import os
 import time
 
-# =========================
-# DHAN CREDENTIALS
-# =========================
 CLIENT_CODE = os.getenv("DHAN_CLIENT_CODE")
 ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
 
-# =========================
-# DHAN CONTEXT
-# =========================
 dhan_context = DhanContext(
     CLIENT_CODE,
     ACCESS_TOKEN
 )
 
-print("===================================")
+print("================================")
 print("🟢 DHAN CONNECTED SUCCESSFULLY")
-print("===================================")
+print("================================")
 
-# =========================
-# BANKNIFTY LIVE DATA
-# =========================
-
-# BankNifty Index
+# BANKNIFTY INDEX
 instruments = [
-    (MarketFeed.NSE_FNO, "25", MarketFeed.Ticker)
+    (MarketFeed.IDX, "25", MarketFeed.Ticker)
 ]
 
 version = "v2"
 
-try:
+feed = MarketFeed(
+    dhan_context,
+    instruments,
+    version
+)
 
-    data = MarketFeed(
-        dhan_context,
-        instruments,
-        version
-    )
+while True:
+    feed.run_forever()
 
-    while True:
+    response = feed.get_data()
 
-        data.run_forever()
+    print("================================")
+    print("⏰", datetime.now().strftime("%H:%M:%S"))
+    print(response)
+    print("================================")
 
-        response = data.get_data()
-
-        print("===================================")
-        print("⏰ TIME :", datetime.now().strftime("%H:%M:%S"))
-        print("📈 BANKNIFTY LIVE DATA")
-        print(response)
-        print("===================================")
-
-        time.sleep(1)
-
-except Exception as e:
-
-    print("ERROR :", e)
+    time.sleep(1)
