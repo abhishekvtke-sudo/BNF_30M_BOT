c52f37ae59a935a7440805df9cd3500cf041d263
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 01:35:19 +0530

    fixed websocketidxxx

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
 from dhanhq import DhanContext, MarketFeed
 import os
+import asyncio
 import time
 
+# =========================
+# DHAN CREDENTIALS
+# =========================
 CLIENT_ID = os.getenv("DHAN_CLIENT_CODE")
 ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
 
+# =========================
+# DHAN CONTEXT
+# =========================
 dhan_context = DhanContext(CLIENT_ID, ACCESS_TOKEN)
 
 print("================================")
 print("🟢 DHAN CONNECTED SUCCESSFULLY")
 print("================================")
 
+# =========================
+# INSTRUMENTS
+# =========================
 instruments = [
     ("IDX_I", "25", MarketFeed.Ticker)
 ]
 
+# =========================
+# MARKET FEED
+# =========================
 feed = MarketFeed(
     dhan_context,
     instruments,
     version="v2"
 )
 
-# CONNECT
-feed.connect()
+# =========================
+# MAIN FUNCTION
+# =========================
+async def start_feed():
 
-while True:
-    try:
-        data = feed.get_data()
+    await feed.connect()
 
-        print("================================")
-        print("📈 LIVE BANKNIFTY DATA")
-        print(data)
-        print("================================")
+    while True:
+        try:
+            data = await feed.get_instrument_data()
 
-        time.sleep(1)
+            print("================================")
+            print("📈 LIVE BANKNIFTY DATA")
+            print(data)
+            print("================================")
 
-    except Exception as e:
-        print("ERROR :", e)
-        time.sleep(5)
+            time.sleep(1)
+
+        except Exception as e:
+            print("ERROR :", e)
+            await asyncio.sleep(5)
+
+# =========================
+# RUN
+# =========================
+asyncio.run(start_feed())
