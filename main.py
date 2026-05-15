7d7011ffec52c61e452f25cb2d54436e27252d1f
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 19:16:17 +0530

    API3

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import os
-import time
-
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
-from dhanhq import DhanContext, dhanhq
-
-# =========================================
-# ENV VARIABLES
-# =========================================
+from dhanhq import marketfeed
 
 client_id = os.environ["DHAN_CLIENT_ID"]
 access_token = os.environ["DHAN_ACCESS_TOKEN"]
@@ -16,53 +7,26 @@ access_token = os.environ["DHAN_ACCESS_TOKEN"]
 print("CLIENT ID LOADED", flush=True)
 print("TOKEN LOADED", flush=True)
 
-# =========================================
-# CONNECT DHAN
-# =========================================
+# BANKNIFTY INDEX
+instruments = [
+    ("IDX_I", "25", 15)
+]
 
-dhan_context = DhanContext(
+def on_connect(instance):
+    print("CONNECTED TO DHAN LIVE FEED", flush=True)
+
+def on_message(instance, message):
+    print("BANKNIFTY DATA :", flush=True)
+    print(message, flush=True)
+
+feed = marketfeed.DhanFeed(
     client_id,
-    access_token
+    access_token,
+    instruments,
+    on_connect=on_connect,
+    on_message=on_message
 )
 
-dhan = dhanhq(dhan_context)
+print("STARTED", flush=True)
 
-print("CONNECTED TO DHAN", flush=True)
-
-# =========================================
-# LIVE LOOP
-# =========================================
-
-while True:
-
-    try:
-
-        print("\n========================", flush=True)
-
-        print(
-            f"TIME : "
-            f"{datetime.now(ZoneInfo('Asia/Kolkata')).strftime('%H:%M:%S')}",
-            flush=True
-        )
-
-        # =====================================
-        # BANKNIFTY INDEX
-        # =====================================
-
-        data = dhan.quote_data(
-            securities={
-                "IDX_I": ["25"]
-            }
-        )
-
-        print("BANKNIFTY DATA :", flush=True)
-
-        print(data, flush=True)
-
-        print("========================", flush=True)
-
-    except Exception as e:
-
-        print(f"ERROR : {e}", flush=True)
-
-    time.sleep(1)
+feed.run_forever()
