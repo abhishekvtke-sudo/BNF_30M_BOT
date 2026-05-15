e9b4b88b244fee1e65d507ca7a67329c64a7a3ff
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 22:12:35 +0530

    API39

--- main.py
+++ main.py
@@ -1,51 +1,7 @@
-import requests
 import time
-import os
 
-print("STEP 1")
-
-CLIENT_ID = os.getenv("DHAN_CLIENT_ID")
-ACCESS_TOKEN = os.getenv("DHAN_ACCESS_TOKEN")
-
-print("STEP 2")
-
-print("CLIENT =", CLIENT_ID)
-
-headers = {
-    "access-token": ACCESS_TOKEN,
-    "client-id": CLIENT_ID,
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
-
-print("STEP 3")
-
-url = "https://api.dhan.co/v2/marketfeed/ltp"
-
-payload = {
-    "MCX_COMM": [491727]
-}
-
-print("STEP 4")
+print("BOT STARTED")
 
 while True:
-
-    print("LOOP START")
-
-    try:
-
-        response = requests.post(
-            url,
-            json=payload,
-            headers=headers
-        )
-
-        print("STATUS =", response.status_code)
-
-        print(response.text)
-
-    except Exception as e:
-
-        print("ERROR =", e)
-
-    time.sleep(15)
+    print("WORKING")
+    time.sleep(10)
