1d1ae8753378cd73a5ded6e9fbf0b66512f4fbce
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 20:16:43 +0530

    API24

--- main.py
+++ main.py
@@ -1,67 +1,9 @@
-import os
 import time
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
-from dhanhq import dhanhq, DhanContext
-
-# =========================================
-# ENV VARIABLES
-# =========================================
-
-client_id = os.environ["DHAN_CLIENT_ID"]
-access_token = os.environ["DHAN_ACCESS_TOKEN"]
-
-print("CLIENT ID LOADED", flush=True)
-print("TOKEN LOADED", flush=True)
-
-# =========================================
-# CONNECT TO DHAN
-# =========================================
-
-dhan_context = DhanContext(
-    client_id,
-    access_token
-)
-
-dhan = dhanhq(dhan_context)
-
-print("CONNECTED TO DHAN", flush=True)
-
-# =========================================
-# LIVE LOOP
-# =========================================
+print("SCRIPT STARTED", flush=True)
 
 while True:
 
-    try:
-
-        print("\n========================", flush=True)
-
-        print(
-            f"TIME : "
-            f"{datetime.now(ZoneInfo('Asia/Kolkata')).strftime('%H:%M:%S')}",
-            flush=True
-        )
-
-        # =====================================
-        # BANKNIFTY LTP
-        # =====================================
-
-        data = dhan.ltp_data(
-            {
-                "NSE_IDX": [25]
-            }
-        )
-
-        print("BANKNIFTY DATA :", flush=True)
-        print(data, flush=True)
-
-        print("========================", flush=True)
-
-    except Exception as e:
-
-        print(f"ERROR : {e}", flush=True)
+    print("LOOP RUNNING", flush=True)
 
     time.sleep(1)
