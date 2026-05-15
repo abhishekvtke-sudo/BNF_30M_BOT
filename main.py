a8cb25c58e4e799bdcb9ddef11249943794560d3
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-15 09:05:52 +0530

    fc2

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import os
 import time
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
+print("STARTED", flush=True)
 
 from Dhan_Tradehull import Tradehull
 
-# =========================================
-# LOAD ENV VARIABLES
-# =========================================
-
-client_id = os.environ["DHAN_CLIENT_ID"]
-access_token = os.environ["DHAN_ACCESS_TOKEN"]
-
-print("CLIENT ID LOADED", flush=True)
-print("TOKEN LOADED", flush=True)
+print("TRADEHULL IMPORTED", flush=True)
 
 # =========================================
-# CONNECT TO DHAN
+# ENV
 # =========================================
 
-print("CONNECTING TO DHAN...", flush=True)
+client_id = os.getenv("DHAN_CLIENT_ID")
+access_token = os.getenv("DHAN_ACCESS_TOKEN")
+
+print("CLIENT ID :", client_id, flush=True)
+print("TOKEN FOUND :", bool(access_token), flush=True)
+
+# =========================================
+# LOGIN
+# =========================================
+
+print("CONNECTING...", flush=True)
 
 tsl = Tradehull(
     client_id,
@@ -28,60 +29,24 @@ tsl = Tradehull(
     mode="access_token"
 )
 
-print("CONNECTED TO DHAN", flush=True)
+print("CONNECTED", flush=True)
 
 # =========================================
-# LIVE LOOP
+# LOOP
 # =========================================
 
 while True:
 
     try:
 
-        print("\n==============================", flush=True)
-
-        print(
-            f"TIME : "
-            f"{datetime.now(ZoneInfo('Asia/Kolkata')).strftime('%H:%M:%S')}",
-            flush=True
-        )
-
-        print("FETCHING LIVE DATA...", flush=True)
-
-        # =====================================
-        # LIVE DATA
-        # =====================================
+        print("FETCHING...", flush=True)
 
         live_data = tsl.get_ltp_data("BANKNIFTY")
 
-        print("RAW DATA RECEIVED", flush=True)
-
-        # =====================================
-        # CHECK DATA
-        # =====================================
-
-        if not live_data:
-
-            print("NO LIVE DATA", flush=True)
-
-            time.sleep(1)
-
-            continue
-
-        # =====================================
-        # EXTRACT LTP
-        # =====================================
-
-        ltp = float(list(live_data.values())[0])
-
-        print(f"LIVE LTP : {ltp}", flush=True)
-
-        print(live_data, flush=True)
-
-        print("==============================", flush=True)
+        print("DATA :", live_data, flush=True)
 
     except Exception as e:
 
-        print(f"ERROR : {e}", flush=True)
+        print("ERROR :", e, flush=True)
 
     time.sleep(5)
