46cfe02e9806d1ecc0fefaae3bbfa2e29de84c68
abhishektvke-sudo <abhishek.vtke@gmail.com>
2026-05-16 17:32:51 +0530

    real7

--- real_trade.py
+++ real_trade.py
@@ -134,7 +134,7 @@ while True:
 
         segment_data = feed_data[first_segment]
 
-        security_data = segment_data[0]
+        security_data = list(segment_data.values())[0]
 
         live_price = float(
            security_data["last_price"]
